5a5460dc4bf6e55ea78d2fa238ef74116f73cb90
504 <504@DESKTOP-IU7TFRJ>
2022-03-11 21:36:47 +0900

    Day8

--- java1/src/Day08/bank/Count.java
+++ java1/src/Day08/bank/Count.java
@@ -3,65 +3,151 @@ package Day08.bank;
 import java.util.Scanner;
 
 public class Count {
-	Scanner scanner = new Scanner(System.in);
+	
+	static Scanner scanner = new Scanner(System.in);
+	static Count [] banker = new Count[2000];
+	
 	// 1. 필드
-	String 계좌개설;
-	int 입금;
-	int 출금;
-	int 이체;
-	int 대출;
-	String 계좌목록;
-	String 이름;
-	int 비밀번호;
-
+	String id;
+	String pw;
+	String name;
+	int money;
 	// 2. 생성자
-	public Count() {}
-	
-	public Count(String 계좌개설, int 입금, int 출금, int 이체, int 대출, String 계좌목록, String 이름, int 비밀번호) {
-		this.계좌개설 = 계좌개설; // ?
-		this.입금 = 입금;
-		this.출금 = 출금;
-		this.이체 = 이체;
-		this.대출 = 대출;
-		this.계좌목록 = 계좌목록;
-		this.이름 = 이름;
-		this.비밀번호 = 비밀번호;
-	}
-	
-	// 3. 메소드
-	boolean 회원메뉴() {
-		System.out.println("-------회원 메뉴-------");
-		System.out.println("1.계좌개설 2.입금 3.출금 4.이체 5.대출 6.계좌목록 선택 : ");
-		int ch = scanner.nextInt();
-		String name = scanner.next();
-		if(ch == 1) {
-			 for(View temp : View.counts) {
-				 if(temp == null && temp.이름.equals(이름)) {
-					 System.out.println("계좌를 개설하였습니다.");
-				 }else {
-					 System.out.println("이름을 다시 한 번 확인해주세요");
-					 
-				 }
-			 }			 
-		}
-		else if(ch == 2) {
-			 for(Count temp : Count.counts) {
-				 if(temp == null && temp.비밀번호 == 비밀번호) {
-					 System.out.println("금액을 넣어주세요. 액수 : ");
-					 int 금액 = scanner.nextInt();
-				 }else {
-					 
-					 
-				 }
-			 }
-		}
-		else if(ch == 3) {}
-		else if(ch == 4) {}
-		else if(ch == 5) {}
-		else if(ch == 6) {}
-		else {}
-		return true;
+	public Count() {
 		
 	}
 	
+	public Count(String id, String pw, String name, int money) {
+		this.id = id;
+		this.pw = pw;
+		this.name = name;
+		this.money = money;
+	}
+	
+	// 3. 메소드
+	public boolean 계좌개설() {
+		// 1. 계좌만들러 왔음
+		// 2. 핸드폰(비대면)
+		// 3. 아이디 계좌 비밀번호 종류를 받는다.
+		// 4. 직원이 만듦
+		// 5. 중복체크
+		// 6. 결과출력
+		System.out.println(" your id : ");
+		String id = scanner.next();
+		System.out.println(" your pw : ");
+		String pw = scanner.next();
+		System.out.println(" your accounttype : ");
+		String name = scanner.next();
+		
+		Count bankers = new Count(id, pw, name, money);
+		
+		for(Count temp : banker) {
+			if(temp != null && temp.id.equals(id)) {
+				System.out.println("알림  : 현재 사용중인 아이디가 있습니다.");
+				return false;
+			}
+		}
+		int i = 0;
+		for(Count temp : banker) {
+			if(temp == null) {
+				banker[i] = bankers;
+				System.out.println("계좌개설이 완료되었습니다.");
+				return true;
+			}
+			i++;
+		}
+		return false;
+	}
+	public void 입금() {
+		// 1. 입금하겠다는 버튼 선택
+		// 2. 금액을 받는다.
+		// 3. 계좌가 있는지 확인한다.
+		// 4. 없으면 종료, 있으면 입금완료
+		// 5. 계좌번호를 배열에 넣는다.
+		// 6. 출금을 한다
+		// 7. 출금한 금액이 계좌에 들어있는 금액보다 적다면 오류발생
+		
+		
+		System.out.println("금액을 입력해주세요. 금액 :");
+		int money = scanner.nextInt();
+		int i = 0;
+		for(Count temp : banker) {
+			if(temp != null) {
+				banker[i].money = money;				
+				System.out.println("입금이 완료되었습니다.");
+				break;
+			}else {
+				System.out.println("계좌번호를 다시 확인해주세요.");
+				
+			}
+			i++;
+		}					
+	}
+	// 1. 출금하겠다는 버튼선택
+			// 2. 출금할 금액을 입력한다.
+			// 3. 비밀번호를 입력한다.
+			// 4. 비밀번호 같은지 확인한다.
+			// 5. 만약에 계좌에 있는 금액보다 더 많은 금액을 입력할경우 오류
+			// 6. 적으면 출금
+			// 7. 출금하였습니다. 출력
+	public void 출금() {
+		System.out.println(" 출금할 금액 : ");
+		int 금액 = scanner.nextInt();
+		int i = 0;
+		System.out.println(" 계좌의 비밀번호를 입력해 주세요 :");
+		String 비번 = scanner.next();
+		for(Count temp : banker) {
+			if(temp != null && temp.pw.equals(pw) ) {
+				banker[i].money = money;
+				System.out.println(" 접속 ");
+			}else {
+				System.out.println(" 비밀번호를 다시 입력해 주세요.");
+				break;
+			}
+		}
+	}
+	// 1. 이체 버튼을 클릭한다.
+	// 2. 상대방 계좌번호를 입력한다.
+	// 3. 비밀번호를 입력한다.
+	// 4. 비밀번호가 맞으면 이체 성공 틀리면 재입력
+	// 5. 이체가 완료되었습니다.
+	public void 이체() {
+		System.out.println(" 계좌번호 입력 : ");
+		int 계좌번호 = scanner.nextInt();
+		int i = 0;
+		System.out.println(" 비밀번호 : ");
+		int 비번 = scanner.nextInt();
+		for(Count temp : banker) {
+			if(temp != null && temp.pw.equals(pw)) {
+				banker[i].money = money;
+				System.out.println(" 이체를 완료했습니다.");
+			}else {
+				System.out.println(" 비밀번호를 다시 입력해주세요");
+				break;
+			}
+		}		
+	}
+	// 1. 금액을 입력한다.
+	// 2. 대출을 클릭한다.
+	// 3. 신용등급이 2등급 이상이면 통과 2등급 미만이면 불가
+	
+	public void 대출() {
+		System.out.println(" 금액 : ");
+		int 금액 = scanner.nextInt();
+		int 신용등급 = 0;
+		while(true) {
+			if(신용등급 >= 2) {
+				System.out.println("대출 완료");
+			}else {
+				System.out.println("신용등급이 2등급 미만이여서 대출이 불가합니다.");
+				break;
+			}
+		}
+	}
+	// 1. 내계좌목록을 클릭한다.
+	// 2. 계좌가 몇 개 있는지 확인한다.
+	public void 내계좌목록() {
+		System.out.print(" 1. 국민계좌 2. 우리계좌 3. ibk계좌 4. 신한계좌 선택 : ");
+		int 선택 = scanner.nextInt();
+	}
 }
--- java1/src/Day08/bank/View.java
+++ java1/src/Day08/bank/View.java
@@ -1,71 +1,67 @@
 package Day08.bank;
 
-import java.util.Scanner;
-
-public class View { // c s
-	
-	static Scanner scanner = new Scanner(System.in);
-	static Count[] counts = new Count[1000];
-	
-	public static void main(String[] args) { // m s
-		
-		View BankOpened = new View();
-		BankOpened.menu();
-		
-	} // m e
-	void menu() { // v s
-		while(true) {			
-			System.out.println("--------회원가입-------");
-			System.out.print(" 1. 회원가입 2. 로그인 3. 아이디찾기 4. 비밀번호찾기 선택 : ");
-			int ch = scanner.nextInt();
-			Model model = new Model();
-			if(ch == 1) { //if
-				boolean result = model.회원가입();	
-				if(result)
-					System.out.println(" 회원가입 성공 ");
-				else
-					System.out.println(" 회원가입 실패 ");
-			}
-			else if(ch == 2) {
-				String result = model.로그인();
-				if(result == null) {
-					System.out.println("동일한 회원 정보가 없습니다.");
-				}else if(result.equals("admin")) {
-					System.out.println(" 안녕하세요! "+result+"님"); // ?
-					adminmenu(); // ?
-				}else {
-					System.out.println(" 안녕하세요! "+result+"님");
-				}
-			}
-			else if(ch == 3) {
-				//model.아이디찾기();
-			}		
-			else if(ch == 4) {
-				//model.비밀번호찾기();
-			}		
-			else {
-				System.out.println("알 수 없는 번호입니다.");
-			}
-		}
-		void Countmenu() {
-			while(true){
-				System.out.println("---------회원 메뉴--------");
-				System.out.print("1.계좌개설 2.입금 3.출금 4.이체 5.대출 6.내 계좌목록 7.취소 선택 : ");
-				int ch = scanner.nextInt();
-				Count count = new Count();
-				if(ch == 1) {Count.계좌개설();}
-				else if(ch == 2) {Count.입금();}
-				else if(ch == 3) {Count.출금();}
-				else if(ch == 4) {Count.이체();}
-				else if(ch == 5) {Count.대출();}
-				else if(ch == 6) {Count.계좌목록();}
-				else if(ch == 7) {return;}
-				else {System.out.println(" 알 수 없는 번호입니다. ");}
-			}
-		}
-	}
-		
-	} // v e
-	
-	
-} // c e
+public class View {// c s
+	  Count count = new Count();
+   
+   
+   public static void main(String[] args) { // m s
+    
+      View BankOpened = new View();
+      BankOpened.menu();
+      
+      
+      
+   } // m e
+   void menu() { // v s
+      while(true) {         
+         System.out.println("--------------------회원가입----------------------");
+         System.out.println(" 1. 회원가입 2. 로그인 3. 아이디찾기 4. 비밀번호찾기       ");
+         System.out.println("------------------------------------------------");
+         System.out.println("번호선택:  ");
+         int ch = Model.scanner.nextInt();
+         Model model = new Model();
+         if(ch == 1) { 
+            boolean result = model.회원가입();   
+            if(result)
+               System.out.println(" 회원가입 성공 ");
+            else
+               System.out.println(" 회원가입 실패 ");
+         }
+         else if(ch == 2) {
+            String result = model.로그인();
+            if(result == null) {
+               System.out.println("동일한 회원 정보가 없습니다.");
+            }else {
+               System.out.println(" 안녕하세요! "+result+"님");
+               Count();
+            }
+         }
+         else if(ch == 3) {
+            //model.아이디찾기();
+         }      
+         else if(ch == 4) {
+            //model.비밀번호찾기();
+         }      
+         else {
+            System.out.println("알 수 없는 번호입니다.");
+         }
+      }
+   }
+   void Count() {
+   while(true){
+       System.out.println("---------회원 메뉴--------");
+       System.out.print("1.계좌개설 2.입금 3.출금 4.이체 5.대출 6.내 계좌목록 7.취소 선택 : ");
+       int ch = Model.scanner.nextInt();                                          
+       if(ch == 1) {count.계좌개설();}
+       else if(ch == 2) {count.입금();}
+       else if(ch == 3) {count.출금();}
+       else if(ch == 4) {count.이체();}
+       else if(ch == 5) {count.대출();}
+       else if(ch == 6) {count.내계좌목록();}
+       else if(ch == 7) {return;}      
+       else {System.out.println(" 알 수 없는 번호입니다. ");}
+    }
+     
+   }     
+      
+} //class end
