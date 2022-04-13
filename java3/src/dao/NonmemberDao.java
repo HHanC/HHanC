02e8d23c2bea61754b097bd81042bfe2343f7ff9
504 <504@DESKTOP-5KAPE2L>
2022-04-13 21:25:20 +0900

    ㄷㅈㅎㅈㄷㅎㄷㅈㅎㄷㅈ

---
+++ java3/src/dao/NonmemberDao.java
@@ -0,0 +1,16 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import com.mysql.cj.protocol.Resultset;
+
+public class NonmemberDao {
+
+	private Connection con;
+	private PreparedStatement ps;
+	private Resultset rs;
+	
+	
+	
+}
